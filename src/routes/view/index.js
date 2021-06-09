8f1420c3b98e8e3799711a12dd99f7d24ed81759
Suraj Verma <surajverma@rentalcars.com>
2021-06-09 21:11:39 +0100

    render signup view

--- src/routes/view/index.js
+++ src/routes/view/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 
 const renderHomePage = require("../../controllers/view/renderHomePage");
+const renderSignupPage = require("../../controllers/view/renderSignupPage");
 
 const router = Router();
 
+router.get("/sign-up", renderSignupPage);
 router.get("/", renderHomePage);
 
 module.exports = router;
