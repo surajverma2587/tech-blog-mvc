4a5edc679d7192a94571fc557803b8aa69536163
Suraj Verma <surajverma@rentalcars.com>
2021-06-09 21:27:12 +0100

    render login view and login auth

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,6 +1,7 @@
 const handleSignupSubmit = () => {
   // POST request with username and password
   // /auth/sign-up
+  // on success window location to /login
 };
 
 console.log("client-side JS");
--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 
 const handleSignUp = require("../../controllers/auth/handleSignUp");
+const handleLogin = require("../../controllers/auth/handleLogin");
 
 const router = Router();
 
 router.post("/sign-up", handleSignUp);
+router.post("/login", handleLogin);
 
 module.exports = router;
