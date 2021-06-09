c467ed94c559b44ac66a239c03ff46d19669c2a2
Suraj Verma <surajverma@rentalcars.com>
2021-06-09 21:37:33 +0100

    view dashboard and auth logout route

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,4 +4,16 @@ const handleSignupSubmit = () => {
   // on success window location to /login
 };
 
+const handleLoginSubmit = () => {
+  // POST request with username and password
+  // /auth/login
+  // on success window location to /dashboard
+};
+
+const handleLogoutClick = () => {
+  // POST request with username and password
+  // /auth/logout
+  // on success window location to /
+};
+
 console.log("client-side JS");
--- src/routes/view/index.js
+++ src/routes/view/index.js
@@ -3,11 +3,14 @@ const { Router } = require("express");
 const renderHomePage = require("../../controllers/view/renderHomePage");
 const renderLoginPage = require("../../controllers/view/renderLoginPage");
 const renderSignupPage = require("../../controllers/view/renderSignupPage");
+const renderDashboardPage = require("../../controllers/view/renderDashboardPage");
 
 const router = Router();
 
 router.get("/sign-up", renderSignupPage);
 router.get("/login", renderLoginPage);
+// add auth middleware here
+router.get("/dashboard", renderDashboardPage);
 router.get("/", renderHomePage);
 
 module.exports = router;
