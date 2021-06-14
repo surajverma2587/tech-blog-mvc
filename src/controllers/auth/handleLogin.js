8dfe1c0d5f06bc5f1bd3f3b927a880c7c41087c3
Suraj Verma <surajverma@rentalcars.com>
2021-06-14 21:56:44 +0100

    controller logic for handle login

--- src/controllers/auth/handleLogin.js
+++ src/controllers/auth/handleLogin.js
@@ -1,9 +1,26 @@
-const handleLogin = (req, res) => {
-  // get data from post body
-  // get the user
-  // if exists validate password and if true then send response
-  // not exist return 404
-  res.json("handle login here");
+const { User } = require("../../models");
+
+const handleLogin = async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const user = await User.findOne({
+    where: {
+      username: username,
+    },
+  });
+
+  if (!user) {
+    console.log("User does not exists");
+    return res.status(401).json({ error: "Failed to login" });
+  }
+
+  if (user.password !== password) {
+    console.log("Incorrect password");
+    return res.status(401).json({ error: "Failed to login" });
+  }
+
+  res.status(200).json({ message: "success" });
 };
 
 module.exports = handleLogin;
