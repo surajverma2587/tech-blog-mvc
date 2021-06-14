a33b9081339b86bffb8472a54f3657df5251d844
Suraj Verma <surajverma@rentalcars.com>
2021-06-14 22:12:01 +0100

    try catch login and signup

--- src/controllers/auth/handleLogin.js
+++ src/controllers/auth/handleLogin.js
@@ -1,26 +1,31 @@
 const { User } = require("../../models");
 
 const handleLogin = async (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
+  try {
+    const username = req.body.username;
+    const password = req.body.password;
 
-  const user = await User.findOne({
-    where: {
-      username: username,
-    },
-  });
+    const user = await User.findOne({
+      where: {
+        username: username,
+      },
+    });
 
-  if (!user) {
-    console.log("User does not exists");
-    return res.status(401).json({ error: "Failed to login" });
+    if (!user) {
+      console.log("User does not exists");
+      return res.status(401).json({ error: "Failed to login" });
+    }
+
+    if (user.password !== password) {
+      console.log("Incorrect password");
+      return res.status(401).json({ error: "Failed to login" });
+    }
+
+    res.status(200).json({ message: "success" });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ error: "Failed to login" });
   }
-
-  if (user.password !== password) {
-    console.log("Incorrect password");
-    return res.status(401).json({ error: "Failed to login" });
-  }
-
-  res.status(200).json({ message: "success" });
 };
 
 module.exports = handleLogin;
--- src/controllers/auth/handleSignUp.js
+++ src/controllers/auth/handleSignUp.js
@@ -1,20 +1,25 @@
 const { User } = require("../../models");
 
 const handleSignUp = async (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
+  try {
+    const username = req.body.username;
+    const password = req.body.password;
 
-  const user = await User.create({
-    username: username,
-    password: password,
-  });
+    const user = await User.create({
+      username: username,
+      password: password,
+    });
 
-  if (!user) {
-    console.log("Failed to create user");
+    if (!user) {
+      console.log("Failed to create user");
+      return res.status(500).json({ error: "Failed to signup" });
+    }
+
+    return res.status(200).json({ message: "success" });
+  } catch (error) {
+    console.log(error.message);
     return res.status(500).json({ error: "Failed to signup" });
   }
-
-  return res.status(200).json({ message: "success" });
 };
 
 module.exports = handleSignUp;
