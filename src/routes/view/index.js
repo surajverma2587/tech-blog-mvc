4aaa248646720d33802c48859f172f5ecb92d9b5
Suraj Verma <surajverma@rentalcars.com>
2021-06-18 19:02:56 +0100

    refactor router to public and private

--- src/routes/view/index.js
+++ src/routes/view/index.js
@@ -2,16 +2,11 @@ const { Router } = require("express");
 
 const auth = require("../../middleware/auth");
 const privateRouter = require("./privateRouter");
-const renderHomePage = require("../../controllers/view/renderHomePage");
-const renderLoginPage = require("../../controllers/view/renderLoginPage");
-const renderSignupPage = require("../../controllers/view/renderSignupPage");
+const publicRouter = require("./publicRouter");
 
 const router = Router();
 
-router.get("/sign-up", renderSignupPage);
-router.get("/login", renderLoginPage);
-router.get("/", renderHomePage);
-
+router.use(publicRouter);
 router.use(auth, privateRouter);
 
 module.exports = router;
