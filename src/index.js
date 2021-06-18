43ec39dade83c87757abafbf251ca464707d46f7
Suraj Verma <surajverma@rentalcars.com>
2021-06-18 19:51:06 +0100

    render dashboard and home with data and css

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sequelize = require("./config/connection");
 const routes = require("./routes");
+const helpers = require("./helpers");
 
 const PORT = process.env.PORT || 3000;
 
@@ -22,7 +23,7 @@ const sessionOptions = {
   }),
 };
 
-const handlebarsOptions = {};
+const handlebarsOptions = { helpers };
 
 const hbs = handlebars.create(handlebarsOptions);
 
